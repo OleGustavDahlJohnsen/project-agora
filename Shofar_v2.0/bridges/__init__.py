c7de1645377e82aa579563425d1dc13a02308026
Ole Gustav Dahl Johnsen <ole.gustav.johnsen@gmail.com>
2025-08-09 02:37:28 +0200

    Create __init__.py
    This file designates the bridges/ directory as a Python sub-package, creating the interoperability layer for the Shofar v2.0 project. Its purpose is to encapsulate the various adapters and plugins that allow the Shofar architecture to communicate with standard, third-party machine learning frameworks. This modular approach ensures that new bridges can be added in the future without altering the core system logic.

---
+++ Shofar_v2.0/bridges/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Bridges Package.
+
+This sub-package contains the interoperability modules that allow the
+Shofar architecture to act as a symbiotic co-processor for standard
+machine learning frameworks like ONNX and PyTorch.
+"""
+print("Bridges package initialized.")
